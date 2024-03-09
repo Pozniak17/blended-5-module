54fd3b8751a63b9295d6edfb5f840b944cb82824
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-03-09 18:53:33 +0000

    searchBar

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,13 +1,45 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import styles from './SearchForm.module.css';
+import { useSearchParams } from 'react-router-dom';
 
-// const regions = [
-//   { id: 'africa', value: 'africa', name: 'Africa' },
-//   { id: 'america', value: 'america', name: 'America' },
-//   { id: 'asia', value: 'asia', name: 'Asia' },
-//   { id: 'europe', value: 'europe', name: 'Europe' },
-//   { id: 'oceania', value: 'oceania', name: 'Oceania' },
-// ];
+const regions = [
+  { id: 'africa', value: 'africa', name: 'Africa' },
+  { id: 'america', value: 'america', name: 'America' },
+  { id: 'asia', value: 'asia', name: 'Asia' },
+  { id: 'europe', value: 'europe', name: 'Europe' },
+  { id: 'oceania', value: 'oceania', name: 'Oceania' },
+];
 
 export const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+
+  const [params, setParams] = useSearchParams();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const region = evt.target.region.value; 
+    
+    setParams({query: region});
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+  <button className={styles.button} type="submit">
+    <FiSearch size="16px" />
+  </button>
+
+  <select
+    aria-label="select"
+    className={styles.select}
+    name="region"
+    required
+    defaultValue={params.get('query') || 'default'}
+  >
+    <option disabled value="default">
+      Select a region
+    </option>
+    {regions.map(({id, value, name}) => 
+      <option key={id} value={value}>{name}</option>)}
+  </select>
+</form>
+  );
 };
--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,10 +1,11 @@
-import { Container, Heading, Section } from 'components';
+import { Container, Heading, SearchForm, Section } from 'components';
 
 export const SearchCountry = () => {
   return (
     <Section>
       <Container>
         <Heading title="SearchCountry" bottom />
+        < SearchForm/>
       </Container>
     </Section>
   );
